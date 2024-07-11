f45856071f905001a7180b815fa09281fc7e7703
chlalstj1999 <alstjsms23@gmail.com>
2024-07-11 07:51:28 +0000

    login exeption revise

--- src/router/find.js
+++ src/router/find.js
@@ -1,15 +1,29 @@
 const router = require("express").Router()
+const customError = require("../const/error")
+const { userNameRegx, emailRegx, idRegx } = require("../const/regx")
 
 router.get("/id", (req, res) => {
     const userName = req.body.userName
-    const phonenumber = req.body.phonenumber
+    const email = req.body.email
 
-    if (userName == "test" && phonenumber == "test") {
-        res.send({
-            "idValue": "testId"
+    try {
+        if (!userName.match(userNameRegx)) {
+            throw customError(400, "이름 형식이 잘못됨")
+        } else if (!email.match(emailRegx)) {
+            throw customError(400, "이메일 형식이 잘못됨")
+        } 
+        
+        if (userName === "최민서" && email === "test12345@example.com") {
+            res.status(200).send({
+                "idValue" : "test12345"
+            })
+        } else {
+            throw customError(404, "계정 정보가 없음")
+        }
+    } catch (err) {
+        res.status(err.statusCode || 500).send({
+            "message" : err.message
         })
-    } else {
-        res.send("존재하지 않는 계정입니다.")
     }
 })
 
@@ -17,12 +31,24 @@ router.get("/pw", (req, res) => {
     const userName = req.body.userName
     const idValue = req.body.idValue
 
-    if (userName == "test" && idValue == "testId") {
-        res.send({
-            "pwValue": "testPw"
+    try {
+        if (!userName.match(userNameRegx)) {
+            throw customError(400, "이름 형식이 잘못됨")
+        } else if (!idValue.match(idRegx)) {
+            throw customError(400, "id 형식이 잘못됨")
+        } 
+        
+        if (userName === "최민서" && idValue === "test12345") {
+            res.status(200).send({
+                "pwValue" : "test12345"
+            })
+        } else {
+            throw customError(404, "계정 정보가 없음")
+        }
+    } catch (err) {
+        res.status(err.statusCode || 500).send({
+            "message" : err.message
         })
-    } else {
-        res.send("존재하지 않는 계정입니다.")
     }
 })
 
