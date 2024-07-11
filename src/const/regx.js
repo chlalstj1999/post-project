1bafad5529202bbbedca27e8c6f1dd6e21ff6e01
chlalstj1999 <alstjsms23@gmail.com>
2024-07-11 08:02:19 +0000

    length revise

--- src/const/regx.js
+++ src/const/regx.js
@@ -1,8 +1,8 @@
-const idRegx = /^[a-zA-Z0-9]{5,20}$/
-const pwRegx = /^(?=.*[a-zA-Z])(?=.*\d).{8,20}$/
+const idRegx = /^[a-zA-Z0-9]{5,15}$/
+const pwRegx = /^(?=.*[a-zA-Z])(?=.*\d).{8,15}$/
 const userNameRegx = /^[가-힣]{2,5}$/
 const emailRegx =  /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/
-const gender = /^[men|women]$/
+const gender = /^[M|W]$/
 const birth = /^(19|20)\d{2}-(0[1-9]|1[0-2])-([0-2][1-9]|3[01])$/
 
 module.exports = { idRegx, pwRegx, userNameRegx, emailRegx, gender, birth }
