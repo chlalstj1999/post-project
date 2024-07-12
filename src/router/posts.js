2539e25bb243a8553dcaae4a29fe5033f8288a80
chlalstj1999 <alstjsms23@gmail.com>
2024-07-12 06:02:50 +0000

    PUT /posts/:postIdx exeption and api add

--- src/router/posts.js
+++ src/router/posts.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
 const customError = require("../const/error")
-const { postTitleRegx, postContetnRegx, commentRegx } = require("../const/regx")
+const { postTitleRegx, postContentRegx, commentRegx } = require("../const/regx")
 
 router.get("/", (req, res) => {
     const accountIdx = req.session.accountIdx
@@ -37,8 +37,23 @@ router.post("/", (req, res) => {
 
     try {
         if (!accountIdx) {
-            throw customError()
+            throw customError(401, "로그인 필요")
+        } else if (!categoryIdx) {
+            throw customError(400, "categoryIdx 값이 안옴")
+        } else if (!title.match(postTitleRegx)) {
+            throw customError(400, "제목 형식 확인 필요")
+        } else if (!content.match(postContentRegx)) {
+            throw customError(400, "내용 형식 확인 필요")
         }
+
+        if (categoryIdx != 1) {
+            throw customError(409, "해당 카테고리가 존재하지 않음")
+        }
+
+        res.status(200).send({
+            //작성한 게시글로 가니까,,,
+            "postIdx" : 1
+        })
     } catch (err) {
         res.status(statusCode || 500).send({
             "message" : err.message
@@ -52,14 +67,27 @@ router.put("/:postIdx", (req, res) => {
     const title = req.body.title
     const content = req.body.content
 
-    if (accountIdx == "") {
-        res.send("로그인 후 이용해주세요.") 
-    } else if (title == "" || content == "") {
-        res.send("제목 및 내용을 입력해주세요.")
-    } else {
-        res.send({
-            "title" : title,
-            "content" : content
+    try {
+        if (!accountIdx) {
+            throw customError(401, "로그인 필요")
+        } else if (!postIdx) {
+            throw customError(400, "postIdx 값이 안옴")
+        } else if (!title.match(postTitleRegx)) {
+            throw customError(400, "제목 형식 확인 필요")
+        } else if (!content.match(postContentRegx)) {
+            throw customError(400, "내용 형식 확인 필요")
+        }
+
+        if (postIdx != 1) {
+            throw customError(409, "해당 게시물이 존재하지 않음")
+        }
+
+        res.status(200).send({
+            "postIdx" : 1
+        })
+    } catch (err) {
+        res.status(statusCode || 500).send({
+            "message" : err.message
         })
     }
 })
