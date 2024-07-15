750ad0f8fda09847ef02d52d429fc2b78abd84d1
chlalstj1999 <alstjsms23@gmail.com>
2024-07-15 04:39:19 +0000

    typo correction

--- src/const/regx.js
+++ src/const/regx.js
@@ -2,11 +2,11 @@ const idRegx = /^[a-zA-Z0-9]{5,15}$/
 const pwRegx = /^(?=.*[a-zA-Z])(?=.*\d).{8,15}$/
 const userNameRegx = /^[가-힣]{2,5}$/
 const emailRegx =  /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/
-const gender = /^[M|W]$/
-const birth = /^(19|20)\d{2}-(0[1-9]|1[0-2])-([0-2][1-9]|3[01])$/
-const categoryNameRegx = /^{1,10}$/
-const postTitleRegx = /^{1,30}$/
-const postContentRegx = /^{1,500}$/
-const commentRegx = /^{1,100}$/
+const genderRegx = /^[M|W]$/
+const birthRegx = /^(19|20)\d{2}-(0[1-9]|1[0-2])-([0-2][1-9]|3[01])$/
+const categoryNameRegx = /^.{1,10}$/
+const postTitleRegx = /^.{1,30}$/
+const postContentRegx = /^.{1,500}$/
+const commentRegx = /^.{1,100}$/
 
-module.exports = { idRegx, pwRegx, userNameRegx, emailRegx, gender, birth, categoryNameRegx, postTitleRegx, postContentRegx, commentRegx }
+module.exports = { idRegx, pwRegx, userNameRegx, emailRegx, genderRegx, birthRegx, categoryNameRegx, postTitleRegx, postContentRegx, commentRegx }
