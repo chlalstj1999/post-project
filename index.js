1feef28a310f1ba3cfc98d62315fde04662e79e2
chlalstj1999 <alstjsms23@gmail.com>
2024-07-15 04:14:07 +0000

    error handler middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mariadb = require("mariadb")
 const session = require("express-session")
+const customError = require("./src/const/error")
 const app = express()
 
 app.use(express.json())
@@ -14,12 +15,50 @@ const pool = mariadb.createPool({
     host: "43.203.229.70",
     user: "stageus",
     password: "1234",
-    database: "post"
+    database: "post",
+    // connectionLimit: 5
 })
 
 // 비동기 함수(asynchronous function) : 실행 중에 다른 작업을 중단하지 않고 실행할 수 있음
 // 오래 걸리는 작업을 기다리지 않고, 다른 것을 먼저 처리하려고 할 때 사용
 // 네트워크 요청, 파일 시스템 엑세스, 데이터베이스 쿼리, 타이머 기반의 작업
+// 만약 동기적으로 처리하면 1. 쿼리 완료될 때까지 코드 실행 멈춤
+// 2. 쿼리 완료될 때까지 기다리므로 응답이 느려지거나 매우 지연
+// 3. 다른 요청을 처리하는 동안 데이터베이스 연결이 끊길 수 있음
+//Promise는 비동기 작업의 단위
+
+// 콜백 함수
+// 흐름 예측 어려움
+// 콜백 지옥 - 콜백 함수가 반복되어 수정도 힘들고 가독성도 떨어짐
+// setTimeout(() => {
+//     console.log("A: 일을 마쳤습니다")
+// }, 2000)
+
+// setTimeout(() => {
+//     console.log("B: 일을 마쳤습니다")
+// }, 1500)
+
+// setTimeout(() => {
+//     console.log("C: 일을 마쳤습니다")
+// }, 1000)
+
+// .then .catch
+// app.get("/", (req, res) => {
+//     pool.getConnection()
+//         .then(conn => {
+//             conn.query("SELECT * FROM role;")
+//                 .then(rows => {
+//                     res.send(rows)
+//                 })
+//                 .catch(err => {
+//                     res.status(500).send(err)
+//                 })
+//         })
+//         .catch(err => {
+//             res.status(500).send(err)
+//         })
+// })
+
 // async 함수는 비동기 함수를 정의할 때 사용되는 키워드
 // await 키워드는 promise가 처리될 때까지 기다림 -> 하나의 작업만 하는 것처럼 보임..?!
 // await은 일반적으로 비동기 작업을 처리할 때 동기적으로 보이게 만들어 줌
@@ -29,9 +68,10 @@ app.get("/", async (req, res) => {
         const rows = await conn.query("SELECT * FROM role;")
         res.send(rows)
     } catch (err) {
-        res.status(500).send(err)
+        res.status(500).send()
     }
   })
+
 // app.use(session({
 //     secure: true,	// https 환경에서만 session 정보를 주고받도록처리
 //     secret: process.env.COOKIE_SECRET, // 암호화하는 데 쓰일 키
@@ -61,6 +101,15 @@ app.use("/posts", postRouter)
 
 const categoryRouter = require("./src/router/categorys")
 app.use("/categorys", categoryRouter)
+
+//정의된 라우터가 없을 때
+app.use((req, res, next) => {
+    next(customError(404, "router not Found"))
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.statusCode || 500).send(err.message)
+})
 
 app.listen(8000, () => {
     console.log("8000번 포트에서 실행")
