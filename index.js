ab64722c7bf68a0e31e0c510a9956bafcda199e1
chlalstj1999 <alstjsms23@gmail.com>
2024-07-15 08:33:14 +0000

    500 error message delete

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ app.use(session({
     saveUninitialized : true
 }))
 
+// app.use(session({
+//     secure: true,	// https 환경에서만 session 정보를 주고받도록처리
+//     secret: process.env.COOKIE_SECRET, // 암호화하는 데 쓰일 키
+//     resave: false, // 세션을 언제나 저장할지 설정함 -> request할 때마다 session을 다시 만들겠냐
+//     saveUninitialized: true, // 세션에 저장할 내역이 없더라도 처음부터 세션을 생성할지 설정 -> 내가 세션 사용 안할거야 근데도 만들겠냐,,
+//     cookie: {	//세션 쿠키 설정 (세션 관리 시 클라이언트에 보내는 쿠키)
+//       httpOnly: true, // 자바스크립트를 통해 세션 쿠키를 사용할 수 없도록 함 -> 웹해킹 막으려고 설정함 () / 무조건 http 통신 외에는 쿠키를 보내지 않겠다
+//       Secure: true https 가 아니면 안보내겠다 / CSRF찾아보기!!(XSS)
+//     }, 세션 - 쿠키
+//     name: 'session-cookie' // 세션 쿠키명 디폴트값은 connect.sid이지만 다른 이름을 줄수도 있다.
+//   }))
+
 const pool = mariadb.createPool({
     host: "43.203.229.70",
     user: "stageus",
@@ -25,6 +37,7 @@ const pool = mariadb.createPool({
 // 만약 동기적으로 처리하면 1. 쿼리 완료될 때까지 코드 실행 멈춤
 // 2. 쿼리 완료될 때까지 기다리므로 응답이 느려지거나 매우 지연
 // 3. 다른 요청을 처리하는 동안 데이터베이스 연결이 끊길 수 있음
+// 하지만 순서 예측 가능! 비동기적으로 처리하면 순서 예측 불가능(뭐가 언제 끝날지 어떻게 앎,,)
 //Promise는 비동기 작업의 단위
 
 // 콜백 함수
@@ -62,27 +75,16 @@ const pool = mariadb.createPool({
 // async 함수는 비동기 함수를 정의할 때 사용되는 키워드
 // await 키워드는 promise가 처리될 때까지 기다림 -> 하나의 작업만 하는 것처럼 보임..?!
 // await은 일반적으로 비동기 작업을 처리할 때 동기적으로 보이게 만들어 줌
-app.get("/", async (req, res) => {
+// 동기적으로 보이게 만들어 줌 + 좀 더 간결한 코드
+app.get("/", async (req, res, next) => {
     try {
         const conn = await pool.getConnection()
         const rows = await conn.query("SELECT * FROM role;")
         res.send(rows)
     } catch (err) {
-        res.status(500).send()
+        next(err)
     }
   })
-
-// app.use(session({
-//     secure: true,	// https 환경에서만 session 정보를 주고받도록처리
-//     secret: process.env.COOKIE_SECRET, // 암호화하는 데 쓰일 키
-//     resave: false, // 세션을 언제나 저장할지 설정함 -> request할 때마다 session을 다시 만들겠냐
-//     saveUninitialized: true, // 세션에 저장할 내역이 없더라도 처음부터 세션을 생성할지 설정 -> 내가 세션 사용 안할거야 근데도 만들겠냐,,
-//     cookie: {	//세션 쿠키 설정 (세션 관리 시 클라이언트에 보내는 쿠키)
-//       httpOnly: true, // 자바스크립트를 통해 세션 쿠키를 사용할 수 없도록 함 -> 웹해킹 막으려고 설정함 () / 무조건 http 통신 외에는 쿠키를 보내지 않겠다
-//       Secure: true https 가 아니면 안보내겠다 / CSRF찾아보기!!(XSS)
-//     }, 세션 - 쿠키
-//     name: 'session-cookie' // 세션 쿠키명 디폴트값은 connect.sid이지만 다른 이름을 줄수도 있다.
-//   }))
 
 const loginRouter = require("./src/router/login")
 app.use("/login", loginRouter)
@@ -101,6 +103,12 @@ app.use("/posts", postRouter)
 
 const categoryRouter = require("./src/router/categorys")
 app.use("/categorys", categoryRouter)
+// 미들웨어 ? 서로 다른 애플리케이션이 서로 통신하는 데 사용되는 소프트웨어
+// 왜 사용하지 ..? 이게 없으면 개발자는 애플리케이션에 연결된 각 소프트웨어 구성 요소의 데이터 교환 모듈을 구축해야 함 == 개발자 편리
+//app.use(미들웨어) : 모든 요청에서 해당 미들웨어 실행
+//app.use('/path', 미들웨어) : path로 시작하는 요청에서 미들웨어 실행
+//app.post('/'path', 미들웨어) : path로 시작하는 POST 요청에서 미들웨어 실행
+//next : 다음 미들웨어 실행
 
 //정의된 라우터가 없을 때
 app.use((req, res, next) => {
