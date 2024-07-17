daf8fb9b2a752c49d47c0877de8c223b38e7db46
chlalstj1999 <alstjsms23@gmail.com>
2024-07-17 07:33:22 +0000

    put categorys router add

--- src/router/categorys.js
+++ src/router/categorys.js
@@ -45,33 +45,33 @@ router.get("/", async (req, res, next) => {
     }
 })
 
-router.put("/:categoryIdx", (req, res, next) => {
-    const accountIdx = req.session.accountIdx
-    const roleIdx = req.session.roleIdx
+router.put("/:categoryIdx", checkLogin, checkRole, async (req, res, next) => {
     const categoryIdx = req.params.categoryIdx
     const categoryName = req.body.categoryName
 
     try {
-        if (!accountIdx) {
-            throw customError(401, "로그인 필요")
-        } else if (roleIdx != admin) {
-            throw customError(403, "관리자가 아니면 수정할 수 없음")
-        } else if (!categoryName.match(categoryNameRegx)) {
+        if (!categoryName.match(categoryNameRegx)) {
             throw customError(400, "카테고리 이름 형식 확인 필요")
         } else if (!categoryIdx) {
             throw customError(400, "categoryIdx 값이 오지 않음")
         }
 
-        if (categoryIdx != 1) {
+        conn = await pool.getConnection()
+        let rows = await conn.query("SELECT * FROM category WHERE idx = ?", [categoryIdx])
+
+        if (rows.length === 0) {
             throw customError(404, "해당 카테고리가 존재하지 않음")
         }
+
+        rows = await conn.query("SELECT * FROM category WHERE name = ?", [categoryName])
     
-        if (categoryName === "야구") {
+        if (rows.length !== 0) {
             throw customError(409, "카테고리가 이미 있음")
         }
+
+        await conn.query("UPDATE category SET name = ? WHERE idx = ?", [categoryName, categoryIdx])
     
         res.status(200).send()
-        console.log(`categoryName : ${categoryName}`)
     } catch (err) {
         next(err)
     }
