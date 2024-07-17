354a155fa9827474d90dcd0862508b21693a4548
chlalstj1999 <alstjsms23@gmail.com>
2024-07-17 06:53:59 +0000

    statuscode -> statusCode

--- src/router/data/error.js
+++ src/router/data/error.js
@@ -1,7 +1,7 @@
 // 특정한 데이터 집합을 반환하고 있으므로 data 파일 만들어서 넣어주기
 const customError = (statusCode, message) => {
     const error = new Error(message)
-    error.statuscode = statusCode
+    error.statusCode = statusCode
     return error
 }
 
