d515c83457c65636a4b67259b5a3b1c58b2c7301
chlalstj1999 <alstjsms23@gmail.com>
2024-07-21 06:03:15 +0000

    post comment service add

--- src/service/comments.js
+++ src/service/comments.js
@@ -1,7 +1,7 @@
 const customError = require("../router/data/error")
 const { commentRegx } = require("../const/regx")
 const { isPost } = require("../repository/posts")
-const { postComment } = require("../repository/comments")
+const { postComment, isComment, putComment } = require("../repository/comments")
 
 let rows = null
 
@@ -20,4 +20,17 @@ const createComment = async (accountIdx, postIdx, comment) => {
     await postComment(accountIdx, postIdx, comment)
 }
 
-module.exports = { createComment }
+const updateComment = async (commentIdx, comment) => {
+    if (!comment.match(commentRegx)) {
+        throw customError(400, "댓글 형식 확인 필요")
+    }
+
+    rows = await isComment(commentIdx)
+    if (!rows) {
+        throw customError(404, "해당 댓글이 존재하지 않음")
+    }
+
+    await putComment(commentIdx, comment)
+}
+
+module.exports = { createComment, updateComment }
