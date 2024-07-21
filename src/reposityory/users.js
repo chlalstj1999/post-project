205f797fa282818ab1b3da3c588c8f61a0a2be86
chlalstj1999 <alstjsms23@gmail.com>
2024-07-21 01:44:10 +0000

    post user repository of 3 layer architecture create

--- src/reposityory/users.js
+++ src/reposityory/users.js
@@ -1,4 +1,5 @@
 const pool = require("../router/db/mariadb")
+const { admin, user } = require("../const/role")
 let conn
 
 const getAccount = async ( idValue, pwValue ) => {
@@ -43,4 +44,43 @@ const getPw = async ( userName, idValue ) => {
     return rows.length !== 0 ? rows : null
 }
 
-module.exports = { getAccount, getId, getPw }
+const getIsDuplicateId = async (idValue) => {
+    let rows = null
+    try {
+        conn = await pool.getConnection()
+        rows = await conn.query("SELECT id FROM account WHERE id = ?", [idValue])
+    } catch(err) {
+        console.log(err)
+    } finally {
+        if (conn) conn.end()
+    }
+
+    return rows.length !== 0 ? rows : null
+}
+
+const getIsDuplicateEmail = async (email) => {
+    let rows = null
+    try {
+        conn = await pool.getConnection()
+        rows = await conn.query("SELECT email FROM account WHERE email = ?", [email])
+    } catch(err) {
+        console.log(err)
+    } finally {
+        if (conn) conn.end()
+    }
+
+    return rows.length !== 0 ? rows : null
+}
+
+const postAccount = async (userName, idValue, pwValue, email, gender, birth) => {
+    try {
+        conn = await pool.getConnection()
+    await conn.query("INSERT INTO account (name, id, password, email, gender, birth, roleIdx) VALUES (?, ?, ?, ?, ?, ?, ?)", [userName, idValue, pwValue, email, gender, birth, user])
+    } catch(err) {
+        console.log(err)
+    } finally {
+        if (conn) conn.end()
+    }
+}
+
+module.exports = { getAccount, getId, getPw, getIsDuplicateId, getIsDuplicateEmail, postAccount }
