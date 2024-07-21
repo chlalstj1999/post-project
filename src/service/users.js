9a93a855c09e71a675677a48c4404b75ac6e2c3f
chlalstj1999 <alstjsms23@gmail.com>
2024-07-21 01:13:09 +0000

    find/id service of 3 layer architecture create

--- src/service/users.js
+++ src/service/users.js
@@ -1,6 +1,6 @@
-const { getAccount } = require("../reposityory/users")
+const { getAccount, getId, getPw } = require("../reposityory/users")
 const customError = require("../router/data/error")
-const { idRegx, pwRegx } = require("../const/regx")
+const { idRegx, pwRegx, userNameRegx, emailRegx } = require("../const/regx")
 
 const validateLogin = async ( idValue, pwValue ) => {
     if (!idValue.match(idRegx)) {
@@ -11,8 +11,6 @@ const validateLogin = async ( idValue, pwValue ) => {
 
     const account = await getAccount(idValue, pwValue)
 
-    console.log(account)
-
     if (!account) {
         throw customError(404, "해당하는 계정 정보가 없습니다")
     }
@@ -20,4 +18,20 @@ const validateLogin = async ( idValue, pwValue ) => {
     return account
 }
 
-module.exports = { validateLogin }
+const selectId = async ( userName, email ) => {
+    if (!userName.match(userNameRegx)) {
+        throw customError(400, "이름 형식이 잘못됨")
+    } else if (!email.match(emailRegx)) {
+        throw customError(400, "이메일 형식이 잘못됨")
+    }
+
+    const account = await getId(userName, email)
+
+    if (!account) {
+        throw customError(404, "계정 정보가 없음")
+    }
+
+    return account
+}
+
+module.exports = { validateLogin, selectId }
