814a577595242b19cbc7ac323011ff20093a34c6
chlalstj1999 <alstjsms23@gmail.com>
2024-07-21 05:33:55 +0000

    like post router add:

--- src/router/posts.js
+++ src/router/posts.js
@@ -1,7 +1,7 @@
 const router = require("express").Router()
 const isLogin = require("../middleware/isLogin")
 const isPostUserMatch = require("../middleware/isPostUserMatch")
-const { selectPosts, createPost, udpatePost, selectPost, deletePost } = require("../service/posts")
+const { selectPosts, createPost, udpatePost, selectPost, deletePost, postLike } = require("../service/posts")
 
 let conn = null
 let rows = null
@@ -71,28 +71,7 @@ router.post("/:postIdx/like", isLogin, async (req, res, next) => {
     const postIdx = req.params.postIdx
 
     try {
-        if (!postIdx) {
-            throw customError(400, "postIdx 값이 안옴")
-        }
-
-        conn = await pool.getConnection()
-        let rows = await conn.query("SELECT * FROM post WHERE idx = ?", [postIdx])
-        if (rows.length === 0) {
-            throw customError(404, "해당 게시물이 존재하지 않음")
-        }
-
-        rows = await conn.query("SELECT * FROM postLike WHERE postIdx = ? AND accountIdx = ?", [postIdx, accountIdx])
-        if (rows.length !== 0) {
-            throw customError(409, "이미 좋아요 누른 유저임")
-        }
-
-        await conn.query("INSERT INTO postLike (postIdx, accountIdx) VALUES (?, ?)", [postIdx, accountIdx])
-        await conn.query(`UPDATE post SET countLike = (
-            SELECT COUNT(*)
-            FROM postLike 
-            JOIN post ON postLike.postIdx = post.idx
-            ) WHERE idx = ?`, [postIdx])
-
+        await postLike(accountIdx, postIdx)
         res.status(200).send()
     } catch (err) {
         next(err)
@@ -106,28 +85,7 @@ router.delete("/:postIdx/like", isLogin, async (req, res, next) => {
     const postIdx = req.params.postIdx
 
     try {
-        if (!postIdx) {
-            throw customError(400, "postIdx 값이 안옴")
-        }
-
-        conn = await pool.getConnection()
-        let rows = await conn.query("SELECT * FROM post WHERE idx = ?", [postIdx])
-        if (rows.length === 0) {
-            throw customError(404, "해당 게시물이 존재하지 않음")
-        }
-
-        rows = await conn.query("SELECT * FROM postLike WHERE postIdx = ? AND accountIdx = ?", [postIdx, accountIdx])
-        if (rows.length === 0) {
-            throw customError(404, "좋아요 누른 유저가 아님")
-        }
-
-        await conn.query("DELETE FROM postLike WHERE postIdx = ? AND accountIdx = ?", [postIdx, accountIdx])
-        await conn.query(`UPDATE post SET countLike = (
-            SELECT COUNT(*)
-            FROM postLike 
-            JOIN post ON postLike.postIdx = post.idx
-            ) WHERE idx = ?`, [postIdx])
-
+        await postLike(accountIdx, postIdx)
         res.status(200).send()
     } catch (err) {
         next(err)
