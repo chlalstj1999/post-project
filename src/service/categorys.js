44f58c6983c9a4c50f41f97b5a7608d5d735a0cc
chlalstj1999 <alstjsms23@gmail.com>
2024-07-21 03:32:12 +0000

    delete category service create

--- src/service/categorys.js
+++ src/service/categorys.js
@@ -1,6 +1,6 @@
 const { categoryNameRegx } = require("../const/regx")
 const customError = require("../router/data/error")
-const { isDuplicateCategory, postCategory, getCategorys } = require("../repository/categorys")
+const { isDuplicateCategory, postCategory, getCategorys, isCategory, putCategory, deleteCategoryRepo } = require("../repository/categorys")
 
 let rows
 
@@ -24,4 +24,39 @@ const selectCategory = async () => {
     return rows
 }
 
-module.exports = { createCategory, selectCategory }
+const updateCategory = async (categoryIdx, categoryName) => {
+    if (!categoryName.match(categoryNameRegx)) {
+        throw customError(400, "카테고리 이름 형식 확인 필요")
+    } else if (!categoryIdx) {
+        throw customError(400, "categoryIdx 값이 오지 않음")
+    }
+
+    rows = await isCategory(categoryIdx)
+    if (!rows) {
+        throw customError(404, "해당 카테고리가 존재하지 않음")
+    }
+
+    rows = await isDuplicateCategory(categoryName) 
+    if (rows) {
+        throw customError(404, "카테고리가 이미 있음")
+    }
+
+    await putCategory(categoryIdx, categoryName)
+}
+
+const deleteCategory = async (categoryIdx) => {
+    if (!categoryIdx) {
+        throw customError(400, "categoryIdx 값이 오지 않음")
+    }
+
+    rows = await isCategory(categoryIdx)
+    if (!rows) {
+        throw customError(404, "해당 카테고리가 존재하지 않음")
+    }
+
+    await deleteCategoryRepo(categoryIdx)
+}
+
+module.exports = { 
+    createCategory, selectCategory, updateCategory, deleteCategory 
+}
