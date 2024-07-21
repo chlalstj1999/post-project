df4c959cf672843f47f7c339d3d5ea0f83121c8a
chlalstj1999 <alstjsms23@gmail.com>
2024-07-21 04:57:44 +0000

    post update service udpate

--- src/service/posts.js
+++ src/service/posts.js
@@ -1,6 +1,6 @@
 const { postTitleRegx, postContentRegx } = require("../const/regx")
 const customError = require("../router/data/error")
-const { getPosts, createPostRepo } = require("../repository/posts")
+const { getPosts, createPostRepo, isPost, putPost } = require("../repository/posts")
 const { isCategory } = require("../repository/categorys")
 
 let rows = null
@@ -37,4 +37,19 @@ const createPost = async (accountIdx, categoryIdx, title, content) => {
     await createPostRepo(accountIdx, categoryIdx, title, content)
 }
 
-module.exports = { selectPosts, createPost }
+const udpatePost = async (postIdx, title, content) => {
+    if (!title.match(postTitleRegx)) {
+        throw customError(400, "제목 형식 확인 필요")
+    } else if (!content.match(postContentRegx)) {
+        throw customError(400, "내용 형식 확인 필요")
+    }
+
+    rows = await isPost(postIdx) 
+    if (!rows) {
+        throw customError(404, "해당 게시물이 존재하지 않음")
+    }
+
+    await putPost(postIdx, title, content)
+}
+
+module.exports = { selectPosts, createPost, udpatePost }
