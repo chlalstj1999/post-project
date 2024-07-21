84669311cee157e5c269cca8583b932d3f6ff924
chlalstj1999 <alstjsms23@gmail.com>
2024-07-21 06:03:33 +0000

    post comment repo add

--- src/repository/comments.js
+++ src/repository/comments.js
@@ -10,8 +10,32 @@ const postComment = async (accountIdx, postIdx, comment) => {
     } catch (err) {
         console.log(err)
     } finally {
-        if (conn) return conn.end()
+        if (conn) conn.end()
     }
 }
 
-module.exports = { postComment }
+const isComment = async (commentIdx) => {
+    try {
+        conn = await pool.getConnection()
+        rows = await conn.query("SELECT idx FROM comment WHERE idx = ?", [commentIdx])
+    } catch (err) {
+        console.log(err)
+    } finally {
+        if (conn) conn.end()
+    }
+
+    return rows.length !== 0 ? rows : null
+}
+
+const putComment = async (commentIdx, comment) => {
+    try {
+        conn = await pool.getConnection()
+        await conn.query("UPDATE comment SET content = ? WHERE idx = ?", [comment, commentIdx])
+    } catch (err) {
+        console.log(err)
+    } finally {
+        if (conn) conn.end()
+    }
+}
+
+module.exports = { postComment, isComment, putComment }
