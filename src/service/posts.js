dc0550c86aea2e14c98ed849de91741eaa6a1865
chlalstj1999 <alstjsms23@gmail.com>
2024-07-21 04:00:32 +0000

    create post service add

--- src/service/posts.js
+++ src/service/posts.js
@@ -1,6 +1,6 @@
 const { postTitleRegx, postContentRegx } = require("../const/regx")
 const customError = require("../router/data/error")
-const { getPosts } = require("../repository/posts")
+const { getPosts, createPostRepo } = require("../repository/posts")
 const { isCategory } = require("../repository/categorys")
 
 let rows = null
@@ -20,4 +20,21 @@ const selectPosts = async (categoryIdx) => {
     return rows
 }
 
-module.exports = { selectPosts }
+const createPost = async (accountIdx, categoryIdx, title, content) => {
+    if (!categoryIdx) {
+        throw customError(400, "categoryIdx 값이 안옴")
+    } else if (!title.match(postTitleRegx)) {
+        throw customError(400, "제목 형식 확인 필요")
+    } else if (!content.match(postContentRegx)) {
+        throw customError(400, "내용 형식 확인 필요")
+    }
+
+    rows = await isCategory(categoryIdx)
+    if (!rows) {
+        throw customError(404, "해당 카테고리가 존재하지 않음")
+    }
+
+    await createPostRepo(accountIdx, categoryIdx, title, content)
+}
+
+module.exports = { selectPosts, createPost }
