f6782c52984c242fa1e2eec45882d9b147449f0d
chlalstj1999 <alstjsms23@gmail.com>
2024-07-21 02:35:33 +0000

    put user info router of 3 layer architecture create

--- src/router/users.js
+++ src/router/users.js
@@ -1,25 +1,24 @@
 const router = require("express").Router()
-const { idRegx, pwRegx, userNameRegx, emailRegx, genderRegx, birthRegx } = require("../const/regx")
 const isLogin = require("../middleware/isLogin")
 const isRole = require("../middleware/isRole")
-const customError = require("./data/error")
-const pool = require("./db/mariadb")
-const { validateLogin, selectId, selectPw, createAccount, selectUsersInfo,
-    updateUserRole
+const { 
+    validateLogin, selectId, selectPw, createAccount, 
+    selectUsersInfo, updateUserRole, selectUserInfo, updateUserInfo
  } = require("../service/users")
 
 let conn
+let rows
 
 router.post("/login", async (req, res, next) => {
     const idValue = req.body.idValue
     const pwValue = req.body.pwValue
 
     try {
-       const account = await validateLogin(idValue, pwValue)
+       rows = await validateLogin(idValue, pwValue)
 
-       req.session.accountIdx = account[0].idx
-       req.session.name = account[0].name
-       req.session.roleIdx = account[0].roleIdx
+       req.session.accountIdx = rows[0].idx
+       req.session.name = rows[0].name
+       req.session.roleIdx = rows[0].roleIdx
        res.status(200).send()
     } catch (err) {
         next(err)
@@ -40,7 +39,7 @@ router.get("/find/id", async (req, res, next) => {
     const email = req.body.email
 
     try {
-        const rows = await selectId(userName, email)
+        rows = await selectId(userName, email)
 
         res.status(200).send({
             "idValue" : rows[0].id
@@ -55,7 +54,7 @@ router.get("/find/pw", async (req, res, next) => {
     const idValue = req.body.idValue
 
     try {
-        const rows = await selectPw(userName, idValue)
+        rows = await selectPw(userName, idValue)
 
         res.status(200).send({
             "pwValue" : rows[0].password
@@ -91,9 +90,9 @@ router.post("/", async (req, res, next) => {
 
 router.get("/", isLogin, isRole, async (req, res, next) => {
     try {
-        const usersInfo = await selectUsersInfo()
+        rows = await selectUsersInfo()
 
-        res.status(200).send(usersInfo)
+        res.status(200).send(rows)
     } catch (err) {
         next(err)
     }
@@ -114,14 +113,10 @@ router.get("/me", isLogin, async (req, res, next) => {
     const accountIdx = req.session.accountIdx
 
     try {
-        conn = await pool.getConnection()
-        const rows = await conn.query("SELECT name AS userName, email, gender, birth FROM account WHERE idx=?", [accountIdx])
-
+        rows = await selectUserInfo(accountIdx)
         res.status(200).send(rows)
     } catch (err) {
         next(err)
-    } finally {
-        if (conn) return conn.end()
     }
 })
 
@@ -133,29 +128,10 @@ router.put("/me", isLogin, async (req, res, next) => {
     const birth = req.body.birth
 
     try {
-        if (!userName.match(userNameRegx)) {
-            throw customError(400, "이름 형식이 잘못됨")
-        } else if (!email.match(email)) {
-            throw customError(400, "이메일 형식이 잘못됨")
-        } else if (!gender.match(gender)) {
-            throw customError(400, "성별 형식이 잘못됨")
-        } else if (!birth.match(birth)) {
-            throw customError(400, "생일 형식이 잘못됨")
-        }
-        
-        conn = await pool.getConnection()
-        const rows = await conn.query("SELECT email FROM account WHERE email = ? AND idx != ?", [email, accountIdx])
-
-        if (rows.length !== 0) {
-            throw customError(409, "이메일 중복")
-        }
-
-        await conn.query("UPDATE account SET name = ?, email = ?, gender = ?, birth = ? WHERE idx = ?", [userName, email, gender, birth, accountIdx])
+        await updateUserInfo(accountIdx, userName, email, gender, birth)
         res.status(200).send()
     } catch (err) {
         next(err)
-    } finally {
-        if (conn) return conn.end()
     }
 })
 
